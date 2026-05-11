6c75dfc64ad315e8958dd7bda74844479f39f306
bkness <DevBrandon@icloud.com>
2026-05-11 09:36:03 -0700

    chore: resolve merge conflicts, take origin versions

--- lights.js
+++
@@ -1,7 +0,0 @@
-    
-const res = await fetch('http://localhost:8000/lights')
-const { data } = await res.json()
-const device = data.devices
-
-const kitchen = device.filter(d => d.deviceName === 'Kitchen')
-kitchen.forEach(d => console.log(d.device))
